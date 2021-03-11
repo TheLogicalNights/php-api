287b60f3c6bba9bd349fab71ba71ddd4b25bbc46
aniket1004 <aniketdhole1004@gmail.com>
2021-03-11 12:05:34 +0530

    Aniket : add update function

--- controller.php
+++ controller.php
@@ -2,35 +2,35 @@
     include ('database.php');
 
     $db = new Database();
-    // if ((!isset($_GET['id'])) && isset($_GET['name']) && isset($_GET['email']))
-    // {
-    //     $name = $_GET['name'];
-    //     $email = $_GET['email'];
-    //     $query = "insert into php_api(name,email) values('$name','$email')";
-    //     $result = $db->insert($query);
-    //     if($result)
-    //     {
-    //         echo "Inserted successfully...";
-    //     }
-    //     else
-    //     {
-    //         echo $result;
-    //     }    
-    // }
-    // if(isset($_GET['id']))
-    // {
-    //     $id = $_GET['id'];
-    //     $query = "delete from php_api where id = ?";
-    //     $result = $db->delete($query,[$id]);
-    //     if($result)
-    //     {
-    //         echo "Deleted successfully...";
-    //     }
-    //     else
-    //     {
-    //         echo $result;
-    //     }
-    // }
+    if ((!isset($_GET['id'])) && isset($_GET['name']) && isset($_GET['email']))
+    {
+        $name = $_GET['name'];
+        $email = $_GET['email'];
+        $query = "insert into php_api(name,email) values('$name','$email')";
+        $result = $db->insert($query);
+        if($result)
+        {
+            echo "Inserted successfully...";
+        }
+        else
+        {
+            echo $result;
+        }    
+    }
+    if(isset($_GET['id']))
+    {
+        $id = $_GET['id'];
+        $query = "delete from php_api where id = ?";
+        $result = $db->delete($query,[$id]);
+        if($result)
+        {
+            echo "Deleted successfully...";
+        }
+        else
+        {
+            echo $result;
+        }
+    }
     if (isset($_GET['id']) && isset($_GET['name']) && isset($_GET['email']))
     {
         $query = "UPDATE `php_api` set `name` = ?, `email`= ? where `id` = ?";
