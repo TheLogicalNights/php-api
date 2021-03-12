4aea8eac2780910fb3913eb04b129b85eeac790d
aniket1004 <aniketdhole1004@gmail.com>
2021-03-12 18:11:46 +0530

    Aniket : add fronted for accept response

---
+++ view/insert.view.php
@@ -0,0 +1,14 @@
+
+<div class="insertForm">
+    <form  method="post">
+        <div>    
+            <label for="fname">Your name</label>
+            <input type="text" name="fname" id="fname" placeholder="Your name">
+        </div>
+        <div>    
+            <label for="email">Your name</label>
+            <input type="email" name="email" id="email" placeholder="Your email">
+        </div>
+        <button type="submit" name="insertRecord">Submit</button>
+    </form>
+</div>
