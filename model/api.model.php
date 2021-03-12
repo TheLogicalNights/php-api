28b7bbab69869ee69cf6e91782f4274ae0fe9232
aniket1004 <aniketdhole1004@gmail.com>
2021-03-12 17:30:27 +0530

    Aniket : add failure response

--- model/api.model.php
+++ model/api.model.php
@@ -20,6 +20,7 @@
             // execute query
             $stmt->execute();
             $result = $stmt->fetchAll();
+            http_response_code(200);
             return $result;
         }
         public function readOne()
@@ -29,6 +30,7 @@
             $stmt->bindParam(1,$this->id);
             $stmt->execute();
             $result = $stmt->fetchAll();
+            http_response_code(200);
             return $result;
         }
         public function insertRecords()
@@ -41,10 +43,12 @@
             // execute query
             if($stmt->execute())
             {
+                http_response_code(200);
                 return true;
             }
             else
             {
+                http_response_code(404);
                 return false;
             }
         }
@@ -57,9 +61,15 @@
             $stmt->bindParam(3,$this->id);
 
             if ($stmt->execute())
+            {
+                http_response_code(200);
                 return true;
+            }
             else
+            {
+                http_response_code(404);
                 return false;
+            }
         }
         public function deleteRecord()
         {
@@ -68,9 +78,15 @@
             $stmt->bindParam(1,$this->id);
             
             if ($stmt->execute())
+            {
+                http_response_code(200);
                 return true;
+            }
             else
+            {
+                http_response_code(404);
                 return false;
+            }
         }
     }
 ?>
