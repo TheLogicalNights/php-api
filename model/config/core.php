5a49056ee0eeb83c9788738bca96266ddb19913f
aniket1004 <aniketdhole1004@gmail.com>
2021-03-16 16:03:35 +0530

    adding jwt config

---
+++ model/config/core.php
@@ -0,0 +1,13 @@
+<?php
+// show error reporting
+error_reporting(E_ALL);
+
+// set your default time-zone
+date_default_timezone_set('Asia/Kolkata');
+
+// variables used for jwt
+$key = "example_key";
+$issued_at = time();
+$expiration_time = $issued_at + (60 * 60); // valid for 1 hour
+$isuser = "http://localhost/php-api/controller/php_api/login.php";
+?>
