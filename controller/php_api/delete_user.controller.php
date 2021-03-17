0869be4a98170ea4e326274c2f7e833a68d414a9
aniket1004 <aniketdhole1004@gmail.com>
2021-03-17 17:06:32 +0530

    Aniket : Updated

---
+++ controller/php_api/delete_user.controller.php
@@ -0,0 +1,33 @@
+<?php
+    include ("C:/xampp/htdocs/php-api/model/config/path.php");
+    include ("$model/config/database.php");
+    include ("$model/user.model.php");
+    include ("$controller/php_api/validate_token.php");
+
+    //created object of database and table
+    $db = new Database();
+    $conn = $db->getConnection();
+    // instantiate product object
+    $user = new user($conn);
+    $data = json_decode(file_get_contents("php://input"));
+    $user->email = isset($data->email) ? $data->email : "";
+    $user->id = isset($data->id) ? $data->id : "";
+    if(!$user->userexists())
+    {
+        http_response_code(401);
+        echo json_encode(array("message" => "Unable to delete user"));
+    }
+    else
+    {
+        if($user->deleteUser())
+        {
+            http_response_code(200);
+            echo json_encode(array("Message" => "User deleted successfully"));
+        }
+        else
+        {
+            http_response_code(401);
+            echo json_encode(array("Message" => "Unable to delete user"));
+        }
+    }
+?>
