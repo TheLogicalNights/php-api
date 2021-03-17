dea4680c1a54493e9c99e5a39aefa174851e21e2
TheLogicalNights <swapniladhav560@gmail.com>
2021-03-17 12:09:20 +0530

    Swapnil : improved

--- model/config/core.php
+++ model/config/core.php
@@ -8,6 +8,6 @@ date_default_timezone_set('Asia/Kolkata');
 // variables used for jwt
 $key = "example_key";
 $issued_at = time();
-$expiration_time = $issued_at + (60 * 60); // valid for 1 hour
+$expiration_time = $issued_at + (86400 * 1); // valid for 1 day
 $isuser = "http://localhost/php-api/controller/php_api/login.php";
 ?>
