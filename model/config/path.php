b15b6d6e50a5f56e0b25272f1b5c2ef905483fdd
aniket1004 <aniketdhole1004@gmail.com>
2021-03-17 17:05:30 +0530

    Aniket : removed unrelated files

--- model/config/path.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $model = "C:/xampp/htdocs/php-api/model";
-    $controller = "C:/xampp\htdocs/php-api/controller";
-    $view = "C:/xampp/htdocs/php-api/view";
-    $jwt = "C:/xampp/htdocs/php-api/libs/php-jwt-master/php-jwt-master/src";
-?>
