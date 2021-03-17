f40e7b6a67ac24157ac1bac6a11105ec9534ab8f
TheLogicalNights <swapniladhav560@gmail.com>
2021-03-17 14:43:08 +0530

    Swapnil : Added global path

--- controller/php_api/create_user.php
+++ controller/php_api/create_user.php
@@ -2,8 +2,9 @@
     // required headers
     header("Access-Control-Allow-Origin: *");
     //include database and table files
-    include ("C:/xampp/htdocs/php-api/model/config/database.php");
-    include ("C:/xampp/htdocs/php-api/model/user.model.php");
+    include ("C:/xampp/htdocs/php-api/model/config/path.php");
+    include ("$model/config/database.php");
+    include ("$model/user.model.php");
 
     //created object of database and table
     $db = new Database();
