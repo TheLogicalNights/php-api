65766b00d1362f6158d0c47dd764a98eea57de91
TheLogicalNights <swapniladhav560@gmail.com>
2021-03-19 15:55:17 +0530

    Swapnil : Path correction has been done

--- controller/php_api/web_traffic/web_traffic.controller.php
+++ controller/php_api/web_traffic/web_traffic.controller.php
@@ -2,9 +2,9 @@
     // required headers
     header("Access-Control-Allow-Origin: *");
     //include database and table files
-    include ("C:/xampp/htdocs/php-api/model/config/path.php");
-    include ("$model/config/database.php");
-    //include ("$model/user.model.php");
+    include ("C:/xampp/htdocs/php-api/model/config/path.model.php");
+    include ("$model/config/database.model.php");
+    include ("$model/traffic.model.php");
 
     $db = new Database();
     $conn = $db->getConnection();
@@ -20,10 +20,10 @@
     {
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
         {
-            $extract($row);
+            extract($row);
             $traffic_record = array(
                 "ip_count" => $ipcount,
-                "session"  => $sessioncount
+                "session_count"  => $sessioncount
             );
             array_push($traffic_details["records"],$traffic_record);
         }
