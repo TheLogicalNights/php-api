5f80f8655b316c3331fe86c9eea4af74a30ec2b7
aniket1004 <aniketdhole1004@gmail.com>
2021-03-19 15:15:54 +0530

    add view traffic apis

--- controller/php_api/web_traffic/web_traffic.controller.php
+++ controller/php_api/web_traffic/web_traffic.controller.php
@@ -10,4 +10,31 @@
     $conn = $db->getConnection();
 
     $trafic = new Trafic($conn);
+
+    $stmt = $trafic->viewTraffic();
+
+    $num = $stmt->rowCount();
+    $traffic_details = array();
+    $traffic_details["records"] = array();
+    if ($num > 0)
+    {
+        while ($row = $stmt->fetch(PDO:FETCH_ASSOC))
+        {
+            $extract($row);
+            $traffic_record = array(
+                "ip_count" => $ipcount,
+                "session"  => $sessioncount
+            ));
+            array_push($traffic_details["records"],$traffic_record);
+        }
+        http_response_code(200);
+        echo json_encode($traffic_details);
+    }    
+    else
+    {
+        http_response_code(404);
+        echo json_encode(array(
+            "message" => "Data not found"
+        ));
+    }
 ?>
