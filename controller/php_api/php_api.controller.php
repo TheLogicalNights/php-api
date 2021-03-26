2a6967c03b218e209698aeff7b5d88abd794bbbb
TheLogicalNights <swapniladhav560@gmail.com>
2021-03-26 12:53:31 +0530

    Updated

--- controller/php_api/php_api.controller.php
+++ controller/php_api/php_api.controller.php
@@ -1,7 +1,11 @@
 <?php
     header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: *");
+
     //include database and table files
-    include ("C:/xampp/htdocs/php-api/model/config/database.php");
+    include ("C:/xampp/htdocs/php-api/model/config/database.model.php");
     include ("C:/xampp/htdocs/php-api/model/api.model.php");
 
     class controller1
