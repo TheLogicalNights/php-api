bde8b52ae75284508ca2ea52e509e19330cac9a1
Swapnil Ramesh Adhav <swapniladhav560@gmail.com>
2021-04-02 13:24:15 +0530

    Path Updated

--- view/readall.view.php
+++ view/readall.view.php
@@ -1,5 +1,6 @@
 <?php
-    include ("C:/xampp/htdocs/php-api/controller/php_api/php_api.controller.php");
+    include ("/var/www/html/php-api/model/config/path.model.php");
+    include ("$controller/php_api/php_api.controller.php");
     $read = new controller1();
     $data=json_decode(file_get_contents("php://input"));
 
