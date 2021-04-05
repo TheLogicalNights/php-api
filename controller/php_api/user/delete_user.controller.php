5434cf65191301de2d0d19af22aa6db2f9be7891
TheLogicalNights <swapniladhav560@gmail.com>
2021-04-05 11:13:28 +0530

    Updated

--- controller/php_api/user/delete_user.controller.php
+++ controller/php_api/user/delete_user.controller.php
@@ -6,7 +6,7 @@
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     
 
-    include ("/var/www/html/php-api/model/config/path.model.php");
+    include ("C:/xampp/htdocs/php-api/model/config/path.model.php");
     include ("$model/config/database.model.php");
     include ("$model/user.model.php");
     //include ("$controller/php_api/login/validate_token.controller.php");
