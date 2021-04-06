7acfafd3a5c240628c3f49bff875a38c58a7d825
TheLogicalNights <swapniladhav560@gmail.com>
2021-04-06 15:39:33 +0530

    Swapnil : Added read category controller

---
+++ controller/php_api/category/read_category.controller.php
@@ -0,0 +1,18 @@
+<?php
+    // required headers
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+    //include database and table files
+    include ("C:/xampp/htdocs/php-api/model/config/path.model.php");
+    include ("$model/config/database.model.php");
+    include ("$model/category.model.php");
+
+    //created object of database and table
+    $db = new Database();
+    $conn = $db->getConnection();
+    // instantiate product object
+    $category = new Category($conn);
+
+    $result = $category->readCategories();
+    echo json_encode(count($result) == 0 ? null : $result);
+?>
