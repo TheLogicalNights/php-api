ba74a3789c67233ec7a38d088f4074e5b50f4352
TheLogicalNights <swapniladhav560@gmail.com>
2021-04-06 15:40:00 +0530

    Swapnil : Added category model

---
+++ model/category.model.php
@@ -0,0 +1,38 @@
+<?php
+    class Category
+    {
+        private $conn;
+        private $table_name = "category";
+
+        public $cat;
+	
+        public function __construct($db)
+        {
+            $this->conn = $db;
+        }
+        public function addCategory()
+        {
+            $query = "INSERT INTO ".$this->table_name."(cat) values(?)";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(1,$this->cat);
+            if($stmt->execute())
+            {   
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public function readCategories()
+        {
+            $query = "SELECT * FROM ".$this->table_name.";";
+            $stmt = $this->conn->prepare($query);
+            // execute query
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+            http_response_code(200);
+            return $result;
+        }
+    }
+?>
