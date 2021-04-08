de8510998081c4b4d6acba437ac84e1de1398bec
TheLogicalNights <swapniladhav560@gmail.com>
2021-04-08 15:05:43 +0530

    Swapnil : added read client controller

---
+++ controller/php_api/client/read_client.controller.php
@@ -0,0 +1,18 @@
+<?php
+    // required headers
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: *");
+    //include database and table files
+    include ("C:/xampp/htdocs/php-api/model/config/path.model.php");
+    include ("$model/config/database.model.php");
+    include ("$model/service.model.php");
+
+    //created object of database and table
+    $db = new Database();
+    $conn = $db->getConnection();
+    // instantiate product object
+    $client = new Clients($conn);
+
+    $result = $client->readClient();
+    echo json_encode(count($result) == 0 ? null : $result);
+?>
