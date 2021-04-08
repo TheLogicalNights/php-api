5185f76334f749ce2a67dadf86a50f9cc5ac8dbb
TheLogicalNights <swapniladhav560@gmail.com>
2021-04-08 14:45:44 +0530

    Solved bugs

--- model/clients.model.php
+++ model/clients.model.php
@@ -1,28 +1,31 @@
 <?php
-    private $conn;
-    private $table_name = "clients";
-
-    public $id;
-    public $name;
-    public $img;
-
-    public function __construct($db)
+    class clients
     {
-        $this->conn = $db;
-    }
-    public function createClient()
-    {
-        $query = "INSERT INTO ".$this->table_name."(name,img) values(?,?)";
-        $stmt = $this->conn->prepare($query);
-        $stmt->bindParam(1,$this->name);
-        $stmt->bindParam(2,$this->img);
-        if($stmt->execute())
-        {   
-            return true;
-        }
-        else
+        private $conn;
+        private $table_name = "clients";
+        
+        public $id;
+        public $name;
+        public $img;
+        
+        public function __construct($db)
         {
-            return false;
+            $this->conn = $db;
         }
+        public function createClient()
+        {
+            $query = "INSERT INTO ".$this->table_name."(name,img) values(?,?)";
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(1,$this->name);
+            $stmt->bindParam(2,$this->img);
+            if($stmt->execute())
+            {   
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }   
     }
 ?>
